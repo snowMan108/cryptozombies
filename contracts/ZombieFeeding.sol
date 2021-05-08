afb4153cb1768648d2ccc1293cdbf6480b391814
snowMan108 <snowmanhi108@gmail.com>
2021-05-08 17:11:07 -0500

    zombie battle system

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -21,6 +21,11 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
+  modifier ownerOf(uint _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    _;
+  }
+
   function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
   }
@@ -33,8 +38,7 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
   }
 
-  function _feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
@@ -49,7 +53,6 @@ contract ZombieFeeding is ZombieFactory {
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    _feedAndMultiply(_zombieId, kittyDna, "kitty");
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
-
 }
